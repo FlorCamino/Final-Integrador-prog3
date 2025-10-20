64619fa97637d76b90c3804829529e22403b39c2
Florencia Camino <flopiis.1994@gmail.com>
2025-10-20 20:56:07 +0200

    Agrego endpoints con roles

--- src/v1/routes/servicios.routes.js
+++ src/v1/routes/servicios.routes.js
@@ -1,15 +1,41 @@
 import express from 'express';
+import { verificarToken } from '../../middlewares/auth.js';
+import { verificarRol } from '../../middlewares/role.js';
 import ServiciosController from '../../controllers/servicios.controller.js';
 
 const serviciosController = new ServiciosController();
 
 const router = express.Router();
 
-router.get('/', serviciosController.obtenerServicios);
-router.get('/:id', serviciosController.obtenerServicioPorId);
-router.put('/modificar/:servicio_id', serviciosController.modificarServicio)
-router.post('/', serviciosController.crearServicio);
-router.delete('/eliminar/:servicio_id', serviciosController.eliminarServicio);
+router.get('/', 
+    verificarToken, 
+    verificarRol('cliente', 'administrador', 'empleado'),
+    serviciosController.obtenerServicios
+);
+
+router.get('/:id', 
+    verificarToken,
+    verificarRol('cliente', 'administrador', 'empleado'),
+    serviciosController.obtenerServicioPorId
+);
+
+router.put('/modificar/:servicio_id', 
+    verificarToken,
+    verificarRol('administrador', 'empleado'),
+    serviciosController.modificarServicio
+);
+
+router.post('/', 
+    verificarToken,
+    verificarRol('administrador', 'empleado'),
+    serviciosController.crearServicio
+);
+
+router.delete('/eliminar/:servicio_id', 
+    verificarToken,
+    verificarRol('administrador', 'empleado'),
+    serviciosController.eliminarServicio
+);
 
 export default router;
 
