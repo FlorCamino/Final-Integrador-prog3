8b25e0d71eb3def60827a033dd271ab5488f0cb4
damianottone <dami.ottone@gmail.com>
2025-10-22 10:25:45 -0300

    pequeños ajustes

--- src/middlewares/attachUsuarioId.js
+++ src/middlewares/attachUsuarioId.js
@@ -2,7 +2,7 @@ export const attachUsuarioId = (req, res, next) => {
   try {
     if (req.usuario && req.usuario.id) req.body.usuario_id = req.usuario.id;
   } catch (e) {
-    // no-op, continue
+    // No hacer nada si ocurre un error
   }
   next();
 };
