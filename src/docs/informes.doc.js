4f9da5d31d890772d3de58311339f476ed8e6125
Franco Challiol <francochalliol23@gmail.com>
2025-10-28 15:23:40 -0300

    Agregue informes.controller

---
+++ src/docs/informes.doc.js
@@ -0,0 +1,35 @@
+/**
+ * @swagger
+ * tags:
+ *   name: Informes
+ *   description: API para la generacion de informes y estadísticas
+ */
+
+/**
+ * @swagger
+ * /api/v1/informes/estadisticas:
+ *   get:
+ *     summary: Obtiene informe general del sistema
+ *     tags: [Informes]
+ *     responses:
+ *       200:
+ *         description: Informe general generado correctamente
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 reservasPorSalon:
+ *                   type: array
+ *                 reservasPorUsuario:
+ *                   type: array
+ *                 resumenReservas:
+ *                   type: object
+ *                 usuariosPorTipo:
+ *                   type: array
+ *                 salones:
+ *                   type: array
+ *                 comentariosPorReserva:
+ *                   type: array
+ */
+
